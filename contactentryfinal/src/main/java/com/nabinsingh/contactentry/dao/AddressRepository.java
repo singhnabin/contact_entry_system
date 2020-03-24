2feb3e30690dcf4b3092d29a8a9a44a8bd059ad6
Nabin <nabinsingh34@gmail.com>
2020-03-24 15:45:04 -0500

    final push

--- contactentryfinal/src/main/java/com/nabinsingh/contactentry/dao/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nabinsingh.contactentry.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nabinsingh.contactentry.entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-
-}
--- contactentryfinal/src/main/java/com/nabinsingh/contactentry/dao/PersonNameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nabinsingh.contactentry.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nabinsingh.contactentry.entity.Name;
-
-public interface PersonNameRepository extends JpaRepository<Name, Integer> {
-
-}
